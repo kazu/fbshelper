kazu/fbshelper

commit 76ae580f86fc18c97255ef1be22fcce8d5c2ff11

Fix fail to wrrite aggreagated ToTable.

query/base/directreader.go
@@ -67,6 +67,12 @@ func NewNoLayer(b Base) NoLayer {
 	return NoLayer{}
 }
 
+// Dup ... return copied Base
+func (b NoLayer) Dup() (dst Base) {
+
+	return NoLayer{BaseImpl: b.BaseImpl.Dup().Impl()}
+}
+
 // Type ... type of Base interface
 func (b NoLayer) Type() uint8 { return BASE_NO_LAYER }
 
@@ -132,11 +138,20 @@ func (b NoLayer) D(off, size int) *Diff {
 	})
 
 	if e == nil && sn >= 0 {
-		if b.Diffs[sn].Offset == off {
-			return &b.Diffs[sn]
+		diff := b.Diffs[sn]
+		loncha.Delete(&b.Diffs, func(i int) bool {
+			return i == sn
+		})
+
+		if diff.Offset == off {
+			b.Diffs = append(b.Diffs, diff)
+			return &b.Diffs[len(b.Diffs)-1]
 		}
-		off_diff := off - b.Diffs[sn].Offset
-		return &Diff{Offset: off, bytes: b.Diffs[sn].bytes[off_diff : off_diff+size]}
+		off_diff := off - diff.Offset
+
+		diff = Diff{Offset: off, bytes: diff.bytes[off_diff : off_diff+size]}
+		b.Diffs = append(b.Diffs, diff)
+		return &b.Diffs[len(b.Diffs)-1]
 	}
 
 	if off+size <= len(b.bytes) {
@@ -262,6 +277,10 @@ func NewDoubleLayer(b Base) DoubleLayer {
 	}
 	return DoubleLayer{}
 
+}
+
+func (b DoubleLayer) Dup() (dst Base) {
+	return DoubleLayer{BaseImpl: b.BaseImpl.Dup().Impl()}
 }
 
 // Type ... type of Base interface

query/base/base_test.go
@@ -2105,6 +2105,7 @@ func Test_ListAdd(t *testing.T) {
 		name        string
 		isFileList  bool
 		isNolayer   bool
+		useMovOff   bool
 		cntOfFile   int
 		startID     uint64
 		incID       uint64
@@ -2113,10 +2114,12 @@ func Test_ListAdd(t *testing.T) {
 		posSplit    int
 	}{
 		// {name , isFileList, {isNolayer, cntOfFile, startID, incID, offsetIndex, nameprefix, posSplit}}
-		{"1,1 filelist", true, true, 2, 10, 10, 2000, "2files", 1},
-		{"5,5 filelist ", true, true, 10, 10, 10, 2000, "10files", 5},
-		{"1,1 recordlist ", false, true, 10, 10, 10, 2000, "10files", 5},
-		{"5,5 recordlist ", false, true, 10, 10, 10, 2000, "10files", 5},
+
+		{"1,1 filelist", true, true, false, 2, 10, 10, 2000, "2files", 1},
+		{"5,5 filelist ", true, true, false, 10, 10, 10, 2000, "10files", 5},
+		{"5,5 filelist ", true, true, true, 10, 10, 10, 2000, "10files", 5},
+		{"1,1 recordlist ", false, true, false, 10, 10, 10, 2000, "10files", 5},
+		{"5,5 recordlist ", false, true, false, 10, 10, 10, 2000, "10files", 5},
 	}
 
 	for _, tt := range tests {
@@ -2131,6 +2134,7 @@ func Test_ListAdd(t *testing.T) {
 				list0 = (*base.List)(MakeRecordList(tt.isNolayer, tt.posSplit, tt.startID, tt.incID, tt.offsetIndex, tt.nameprefix).CommonNode)
 				list1 = (*base.List)(MakeRecordList(tt.isNolayer, cnt, tt.startID*2, tt.incID*2, tt.offsetIndex*2, "2"+tt.nameprefix).CommonNode)
 			}
+			base.OptUseMovOff(tt.useMovOff)(&base.CurrentGlobalConfig)
 
 			e := list0.Add(list1)
 			assert.NoError(t, e, "fail list0.Add(*list1)")

commit 5496a39cd3725542fe870bb233a251b3a404f305

Fix lost of test
fixes: 76ae580f86fc18c97255ef1be22fcce8d5c2ff11

query/base/directreader.go
@@ -139,19 +139,17 @@ func (b NoLayer) D(off, size int) *Diff {
 
 	if e == nil && sn >= 0 {
 		diff := b.Diffs[sn]
-		loncha.Delete(&b.Diffs, func(i int) bool {
-			return i == sn
-		})
 
 		if diff.Offset == off {
+			loncha.Delete(&b.Diffs, func(i int) bool {
+				return i == sn
+			})
 			b.Diffs = append(b.Diffs, diff)
 			return &b.Diffs[len(b.Diffs)-1]
 		}
 		off_diff := off - diff.Offset
 
-		diff = Diff{Offset: off, bytes: diff.bytes[off_diff : off_diff+size]}
-		b.Diffs = append(b.Diffs, diff)
-		return &b.Diffs[len(b.Diffs)-1]
+		return &Diff{Offset: off, bytes: b.Diffs[sn].bytes[off_diff : off_diff+size]}
 	}
 
 	if off+size <= len(b.bytes) {