kazu/fbshelper

commit cec7da7493b0b37caecc2fe9da456c9248443429

add Implt.mergeDiffs() FIx  no-decrecing cap of prev buf in insertSpace()
Fix expand len using shared in D()

query/base/base.go
@@ -224,6 +224,32 @@ func (d Diff) Innerd(pos, size int) bool {
 	return false
 }
 
+// MergeStable ... keep nodata hole
+func (d *Diff) MergeStable(s *Diff) bool {
+
+	if d.Offset+len(d.bytes) < s.Offset {
+		return false
+	}
+
+	if s.Offset+len(s.bytes) < d.Offset {
+		return false
+	}
+
+	off := MinInt(d.Offset, s.Offset)
+	loff := MaxInt(d.Offset+len(d.bytes), s.Offset+len(s.bytes))
+
+	nDiff := Diff{Offset: off, bytes: make([]byte, loff-off)}
+
+	copy(nDiff.bytes[d.Offset-off:], d.bytes)
+	copy(nDiff.bytes[s.Offset-off:], s.bytes)
+
+	d.Offset = nDiff.Offset
+	d.bytes = nDiff.bytes
+
+	return true
+
+}
+
 func (d *Diff) Merge(s *Diff) {
 
 	var nbytes []byte
@@ -524,10 +550,11 @@ func (b *BaseImpl) writerR(off int, opts ...OptIO) []byte {
 	}
 
 	if len(b.bytes) < off {
-		Log(LOG_WARN, func() LogArgs {
-			return F("base.R(): remain offset=%d lenBuf()=%d \n",
+		Log2(L2_WARN_IO, L2F(func() {
+			FF("base.R(): remain offset=%d lenBuf()=%d \n",
 				off, b.LenBuf())
-		})
+		}))
+
 		return nil
 	}
 
@@ -648,10 +675,13 @@ func (b *BaseImpl) Copy(src IO, srcOff, size, dstOff, extend int) {
 		data := src.R(srcPtr, Size(size-(srcPtr-srcOff)))
 		if len(data) == 0 {
 			//FIXME: if raise this warning. code may have bugs.
-			Log(LOG_WARN, func() LogArgs {
-				return F("BaseImpl.Copy() dont have srcOff=%d in srcOff.LenBuf()=%d\n",
-					srcPtr, src.LenBuf())
-			})
+
+			Log2(L2_WARN_IO,
+				L2F(func() {
+					FF("BaseImpl.Copy() dont have srcOff=%d in srcOff.LenBuf()=%d\n",
+						srcPtr, src.LenBuf())
+				}),
+			)
 			break
 		}
 		if len(data) > size-(srcPtr-srcOff) {
@@ -856,6 +886,39 @@ func (b *BaseImpl) Merge() {
 	b.Flatten()
 }
 
+func (b *BaseImpl) MergeDiffs() {
+	b.mergeDiffs(false)
+}
+func (b *BaseImpl) mergeDiffs(enableRdiffs bool) {
+
+	// if enableRdiffs {
+	// 	for i := 0; i < len(b.RDiffs); i++ {
+	// 		b.Diffs[0].Merge(&b.RDiffs[i])
+	// 	}
+	// }
+	deleteIdxs := map[int]bool{}
+
+	o := b.R(8)
+
+	for i := 1; i < len(b.Diffs); i++ {
+		success := b.Diffs[0].MergeStable(&b.Diffs[i])
+		if success {
+			deleteIdxs[i] = true
+		}
+	}
+	if len(deleteIdxs) == 0 {
+		return
+	}
+	loncha.Delete(&b.Diffs, func(i int) bool {
+		return deleteIdxs[i]
+	})
+	a := b.R(8)
+	_, _ = a, o
+
+	return
+
+}
+
 // Flatten ... Diffs buffer join to bytes
 func (b *BaseImpl) Flatten() {
 	b.FlattenWithLen(-1)
@@ -971,7 +1034,7 @@ func (b *BaseImpl) insertSpace(pos, size int, isCreate bool) IO {
 		if diff.Offset < pos && diff.Include(pos) {
 			newBase.Diffs = append(newBase.Diffs[:i],
 				append([]Diff{
-					Diff{Offset: diff.Offset, bytes: diff.bytes[:pos-diff.Offset]},
+					Diff{Offset: diff.Offset, bytes: diff.bytes[: pos-diff.Offset : pos-diff.Offset]},
 					Diff{Offset: pos, bytes: diff.bytes[pos-diff.Offset:]}},
 					b.Diffs[i+1:]...)...)
 		}

query/base/directreader.go
@@ -128,9 +128,9 @@ func (b NoLayer) D(off, size int) *Diff {
 			return &b.Diffs[len(b.Diffs)-1]
 		}
 		diff := b.Diffs[sn]
-		off_diff := off - diff.Offset
-
-		return &Diff{Offset: off, bytes: b.Diffs[sn].bytes[off_diff : off_diff+size]}
+		offDiff := off - diff.Offset
+		b.Diffs[sn].bytes = b.Diffs[sn].bytes[:offDiff+size]
+		return &Diff{Offset: off, bytes: b.Diffs[sn].bytes[offDiff : offDiff+size]}
 	}
 
 	if off+size <= len(b.bytes) {

query/base/common.go
@@ -296,7 +296,8 @@ RESULT:
 	// b := cNode.Base
 	// _, _ = a, b
 
-	if !IsStructName[node.Name] && !node.VirtualTableIsZero(idx) && !node.IO.Impl().Equal(cNode.IO.Impl()) {
+	//FXIME: enable debug mode ?
+	if L2isEnable(L2_DEBUG_IO) && !IsStructName[node.Name] && !node.VirtualTableIsZero(idx) && !node.IO.Impl().Equal(cNode.IO.Impl()) {
 		Log(LOG_DEBUG, func() LogArgs {
 			return F("FieldAt: Invalid Node=%s idx=%d\n", node.Name, idx)
 		})

query/base/log.go
@@ -1,6 +1,11 @@
 package base
 
 import (
+	"fmt"
+	"io"
+	"os"
+	"strings"
+
 	log "github.com/kazu/fbshelper/query/log"
 )
 
@@ -8,11 +13,13 @@ const (
 	FLT_NONE = 1 << iota
 	FLT_NORMAL
 	FLT_IS
+	FLT_IO
 )
 
 type LogState struct {
 	level  log.LogLevel
 	filter byte
+	writer io.Writer
 }
 
 var LogCurrentState LogState = LogState{level: log.LOG_ERROR, filter: FLT_NORMAL}
@@ -23,7 +30,7 @@ type LogOptParam struct {
 	state  *LogState
 	level  log.LogLevel
 	filter byte
-	logfns []LogFn
+	logfns []func()
 	fn     func() L2Run
 }
 
@@ -38,7 +45,7 @@ func L2OptFlag(level log.LogLevel, filter byte) Log2Option {
 
 func L2OptF(fn LogFn) Log2Option {
 	return func(p *LogOptParam) {
-		p.logfns = append(p.logfns, fn)
+		p.logfns = append(p.logfns, func() { fn() })
 	}
 }
 
@@ -53,11 +60,14 @@ func SetL2Current(level log.LogLevel, filter byte) {
 
 	LogCurrentState.level = level
 	LogCurrentState.filter = filter
+	if LogCurrentState.level == LOG_DEBUG {
+		os.Exit(0)
+	}
 
 }
 
 func L2isEnable(param Log2Option) bool {
-	logParam := LogOptParam{state: &LogCurrentState, filter: FLT_NONE, logfns: make([]LogFn, 0, 2)}
+	logParam := LogOptParam{state: &LogCurrentState, filter: FLT_NONE, logfns: make([]func(), 0, 2)}
 
 	param(&logParam)
 
@@ -73,7 +83,7 @@ func L2isEnable(param Log2Option) bool {
 
 // Log2 ... logger with lazy evaluation and filter mode
 func Log2(opts ...Log2Option) (result interface{}) {
-	logParam := LogOptParam{state: &LogCurrentState, filter: FLT_NONE, logfns: make([]LogFn, 0, 2)}
+	logParam := LogOptParam{state: &LogCurrentState, filter: FLT_NONE, logfns: make([]func(), 0, 2)}
 
 	for _, opt := range opts {
 		opt(&logParam)
@@ -88,7 +98,7 @@ func Log2(opts ...Log2Option) (result interface{}) {
 		}
 		if enableAfterLog {
 			SetLogLevel(logParam.state.level)
-			Log(logParam.level, logParam.logfns[1])
+			neoLog(logParam.level, logParam.logfns[1])
 			SetLogLevel(o)
 		}
 	}()
@@ -100,7 +110,7 @@ func Log2(opts ...Log2Option) (result interface{}) {
 	SetLogLevel(logParam.state.level)
 	defer SetLogLevel(o)
 	if len(logParam.logfns) > 0 {
-		Log(logParam.level, logParam.logfns[0])
+		neoLog(logParam.level, logParam.logfns[0])
 	}
 	if len(logParam.logfns) >= 2 {
 		enableAfterLog = true
@@ -110,6 +120,9 @@ func Log2(opts ...Log2Option) (result interface{}) {
 
 var (
 	L2_DEBUG_IS Log2Option = L2OptFlag(LOG_DEBUG, FLT_IS)
+	L2_WARN_IS  Log2Option = L2OptFlag(LOG_WARN, FLT_IS)
+	L2_WARN_IO  Log2Option = L2OptFlag(LOG_WARN, FLT_IO)
+	L2_DEBUG_IO Log2Option = L2OptFlag(LOG_DEBUG, FLT_IO)
 )
 
 type LogFmtParam func() []interface{}
@@ -117,13 +130,52 @@ type LogFmtParam func() []interface{}
 func L2fmt(s string, vlist ...interface{}) Log2Option {
 
 	return L2OptF(func() LogArgs {
-		return F(s, vlist...)
+		FF(s, vlist...)
+		return LogArgs{}
 	})
 }
 
-func L2Fmt(s string, fn LogFmtParam) Log2Option {
+// func L2Fmt(s string, fn LogFmtParam) Log2Option {
+
+// 	return L2OptF(func() LogArgs {
+// 		return F(s, fn())
+// 	})
+// }
+
+func L2F(fn func()) Log2Option {
 
 	return L2OptF(func() LogArgs {
-		return F(s, fn())
+		fn()
+		return LogArgs{}
 	})
+}
+
+func FF(s string, v ...interface{}) {
+	fmt.Fprintf(LogCurrentState.writer, s, v...)
+}
+
+func neoLog(l LogLevel, fn func()) {
+
+	if CurrentLogLevel < l {
+		return
+	}
+	o := LogW
+
+	var b strings.Builder
+	LogW = &b
+
+	switch l {
+	case LOG_DEBUG:
+		b.WriteString("D: ")
+	case LOG_WARN:
+		b.WriteString("W: ")
+	case LOG_ERROR:
+		b.WriteString("E: ")
+	default:
+		b.WriteString(" ")
+	}
+	fn()
+	io.WriteString(LogW, b.String())
+	LogW = o
+
 }

commit a7c628d9fd765327137765522de519e680bd4c25

Fix enbug Nolayer.D()
Improve mergeDiffs.

fixes: cec7da7493b0b37caecc2fe9da456c9248443429

query/base/base.go
@@ -889,7 +889,18 @@ func (b *BaseImpl) Merge() {
 func (b *BaseImpl) MergeDiffs() {
 	b.mergeDiffs(false)
 }
+
 func (b *BaseImpl) mergeDiffs(enableRdiffs bool) {
+
+LOOP:
+	for i := 0; i < len(b.Diffs)-1; i++ {
+		if b.mergeDiffsOf(i, enableRdiffs) {
+			goto LOOP
+		}
+	}
+}
+
+func (b *BaseImpl) mergeDiffsOf(idx int, enableRdiffs bool) bool {
 
 	// if enableRdiffs {
 	// 	for i := 0; i < len(b.RDiffs); i++ {
@@ -898,24 +909,24 @@ func (b *BaseImpl) mergeDiffs(enableRdiffs bool) {
 	// }
 	deleteIdxs := map[int]bool{}
 
-	o := b.R(8)
+	//o := b.R(8)
 
-	for i := 1; i < len(b.Diffs); i++ {
-		success := b.Diffs[0].MergeStable(&b.Diffs[i])
+	for i := idx + 1; i < len(b.Diffs); i++ {
+		success := b.Diffs[idx].MergeStable(&b.Diffs[i])
 		if success {
 			deleteIdxs[i] = true
 		}
 	}
 	if len(deleteIdxs) == 0 {
-		return
+		return false
 	}
 	loncha.Delete(&b.Diffs, func(i int) bool {
 		return deleteIdxs[i]
 	})
-	a := b.R(8)
-	_, _ = a, o
+	//a := b.R(8)
+	//_, _ = a, o
 
-	return
+	return true
 
 }
 
@@ -1047,9 +1058,11 @@ func (b *BaseImpl) insertSpace(pos, size int, isCreate bool) IO {
 	}
 
 	if len(newBase.bytes) > pos {
-		newBase.Diffs = append(newBase.Diffs,
-			Diff{Offset: pos + size, bytes: newBase.bytes[pos:]})
-		newBase.bytes = newBase.bytes[:pos]
+		newBase.Diffs = append(
+			append(make([]Diff, 0, len(newBase.Diffs)),
+				Diff{Offset: pos + size, bytes: newBase.bytes[pos:]}),
+			newBase.Diffs...)
+		newBase.bytes = newBase.bytes[:pos:pos]
 	}
 	if isCreate {
 		newBase.Diffs = append(newBase.Diffs,

query/base/directreader.go
@@ -129,7 +129,6 @@ func (b NoLayer) D(off, size int) *Diff {
 		}
 		diff := b.Diffs[sn]
 		offDiff := off - diff.Offset
-		b.Diffs[sn].bytes = b.Diffs[sn].bytes[:offDiff+size]
 		return &Diff{Offset: off, bytes: b.Diffs[sn].bytes[offDiff : offDiff+size]}
 	}
 