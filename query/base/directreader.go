kazu/fbshelper

commit ec9d4af8f52a1a6366525c4ac13a0f329dab260f

Add NoLayer as Base Buffer.
Fix CommonList  replace BaseImpl to Base.

Add Benchmark for NoLayer Buffer.

query/base/directreader.go
@@ -4,6 +4,7 @@ import (
 	"io"
 
 	log "github.com/kazu/fbshelper/query/log"
+	"github.com/kazu/loncha"
 )
 
 type DirectReader struct {
@@ -36,4 +37,143 @@ func (b DirectReader) R(offset int) (result []byte) {
 
 func (b DirectReader) ShouldCheckBound() bool {
 	return false
+}
+
+type NoLayer struct {
+	*BaseImpl
+}
+
+func NewNoLayer(b Base) NoLayer {
+	impl := b.(*BaseImpl)
+	return NoLayer{BaseImpl: impl}
+}
+
+func (b NoLayer) insertBuf(pos, size int) Base {
+	return b.insertSpace(pos, size, true)
+}
+
+func (b NoLayer) insertSpace(pos, size int, isCreate bool) Base {
+	nImple := b.BaseImpl.insertSpace(pos, size, isCreate).(*BaseImpl)
+	if nImple.LenBuf() >= pos+size {
+		b.BaseImpl = nImple
+	} else {
+		b.BaseImpl = b.BaseImpl.insertSpace(pos, size, true).(*BaseImpl)
+	}
+
+	return b
+}
+
+func (b NoLayer) R(off int) []byte {
+
+	if off < len(b.bytes) {
+		return b.bytes[off:]
+	}
+
+	sn, e := loncha.LastIndexOf(b.Diffs, func(i int) bool {
+		return b.Diffs[i].Offset <= off && off < (b.Diffs[i].Offset+len(b.Diffs[i].bytes))
+	})
+	if e == nil && sn >= 0 {
+		return b.Diffs[sn].bytes[off-b.Diffs[sn].Offset:]
+	}
+
+	if off < cap(b.bytes) {
+		if b.expandBuf(off-len(b.bytes)) == nil {
+			return b.bytes[off:]
+		}
+	}
+
+	log.Log(LOG_WARN, log.Printf("NoyLayer.R(%d) invalid len(bytes)=%d cap(bytes)=%d\n",
+		off, len(b.bytes), cap(b.bytes)))
+
+	newDiff := Diff{Offset: cap(b.bytes), bytes: make([]byte, 0, 512)}
+	b.Diffs = append(b.Diffs, newDiff)
+
+	return newDiff.bytes[off-cap(b.bytes):]
+
+}
+
+func (b NoLayer) D(off, size int) *Diff {
+
+	sn, e := loncha.LastIndexOf(b.Diffs, func(i int) bool {
+		return b.Diffs[i].Offset <= off && off < (b.Diffs[i].Offset+cap(b.Diffs[i].bytes))
+	})
+
+	if e == nil && sn >= 0 {
+		if b.Diffs[sn].Offset == off {
+			return &b.Diffs[sn]
+		}
+		off_diff := off - b.Diffs[sn].Offset
+		return &Diff{Offset: off, bytes: b.Diffs[sn].bytes[off_diff:]}
+	}
+
+	if off+size <= len(b.bytes) {
+		return &Diff{Offset: off, bytes: b.bytes[off:]}
+	}
+
+	if off+size <= cap(b.bytes) {
+		b.bytes = b.bytes[:off+size]
+
+		return &Diff{Offset: off, bytes: b.bytes[off:]}
+	}
+
+	newDiff := Diff{Offset: off, bytes: make([]byte, 0, 512)}
+	newDiff.bytes = newDiff.bytes[:size]
+	b.Diffs = append(b.Diffs, newDiff)
+
+	return &newDiff
+}
+
+func (b NoLayer) U(off, size int) []byte {
+
+	diff := b.D(off, size)
+	if cap(b.bytes) < size {
+		diff.bytes = make([]byte, size)
+	}
+	return diff.bytes
+}
+
+func (b NoLayer) Copy(osrc Base, srcOff, size, dstOff, extend int) {
+
+	// src, ok := osrc.(*BaseImpl)
+	// _ = src
+	// if !ok {
+	// 	log.Log(LOG_WARN, log.Printf("BaseImpl.Copy() src is only BaseImpl"))
+
+	// 	return
+	// }
+	if cap(b.bytes) > dstOff {
+		diff := Diff{Offset: dstOff, bytes: b.bytes[dstOff:]}
+		b.Diffs = append(b.Diffs, diff)
+		b.bytes = b.bytes[:dstOff:dstOff]
+	}
+
+	for i, diff := range b.Diffs {
+		if diff.Offset >= dstOff {
+			diff.Offset += extend
+		}
+		b.Diffs[i] = diff
+	}
+	srcDiffs := append([]Diff{Diff{Offset: 0, bytes: osrc.R(0)}},
+		osrc.GetDiffs()...)
+
+	loncha.Delete(&srcDiffs, func(i int) bool {
+		return srcDiffs[i].Offset > srcOff+size || srcDiffs[i].Offset+len(srcDiffs[i].bytes) < srcOff
+	})
+
+	for _, diff := range srcDiffs {
+		diff.Offset += dstOff - srcOff
+		loncha.Delete(&b.Diffs, func(i int) bool {
+			return diff.Offset <= b.Diffs[i].Offset &&
+				b.Diffs[i].Offset+len(b.Diffs[i].bytes) <= diff.Offset+len(diff.bytes)
+		})
+		if diff.Offset > 0 {
+			b.Diffs = append(b.Diffs, diff)
+		}
+	}
+
+}
+func (b NoLayer) New(bytes []byte) Base {
+
+	return NewNoLayer(NewBase(bytes))
+
 }

query/base/base.go
@@ -91,9 +91,9 @@ type Base interface {
 	Next(skip int) Base
 	HasIoReader() bool
 	R(off int) []byte
+	D(off, size int) *Diff
 	C(off, size int, src []byte) error
 	Copy(src Base, srcOff, size, dstOff, extend int)
-	D(off, size int) *Diff
 	U(off, size int) []byte
 	LenBuf() int
 	Merge()
@@ -105,6 +105,7 @@ type Base interface {
 	GetDiffs() []Diff
 	SetDiffs([]Diff)
 	ShouldCheckBound() bool
+	New([]byte) Base
 }
 
 // BaseImpl ... Base Object of byte buffer for flatbuffers
@@ -150,6 +151,14 @@ func NewBase(buf []byte) *BaseImpl {
 func NewBaseByIO(rio io.Reader, cap int) *BaseImpl {
 	b := &BaseImpl{r: rio, bytes: make([]byte, 0, cap)}
 	return b
+}
+
+// func (b *BaseImpl) New(bytes []byte) Base {
+// 	return nil
+// }
+
+func (b *BaseImpl) New(bytes []byte) Base {
+	return NewBase(bytes)
 }
 
 // NextBase provide next root flatbuffers
@@ -234,14 +243,14 @@ func (b *BaseImpl) C(off, size int, src []byte) error {
 	return nil
 }
 
-func (b *BaseImpl) Copy(osrc Base, srcOff, size, dstOff, extend int) {
+func (b *BaseImpl) Copy(src Base, srcOff, size, dstOff, extend int) {
 
-	src, ok := osrc.(*BaseImpl)
-	if !ok {
-		log.Log(LOG_WARN, log.Printf("BaseImpl.Copy() src is only BaseImpl"))
+	// src, ok := osrc.(*BaseImpl)
+	// if !ok {
+	// 	log.Log(LOG_WARN, log.Printf("BaseImpl.Copy() src is only BaseImpl"))
 
-		return
-	}
+	// 	return
+	// }
 
 	if len(b.bytes) > dstOff {
 		diff := Diff{Offset: dstOff, bytes: b.bytes[dstOff:]}
@@ -256,16 +265,16 @@ func (b *BaseImpl) Copy(osrc Base, srcOff, size, dstOff, extend int) {
 		b.Diffs[i] = diff
 	}
 
-	if len(src.bytes) > srcOff {
-		nSize := len(src.bytes[srcOff:])
+	if len(src.R(0)) > srcOff {
+		nSize := len(src.R(0)[srcOff:])
 		if nSize > size {
 			nSize = size
 		}
 
-		diff := Diff{Offset: dstOff, bytes: src.bytes[srcOff : srcOff+nSize]}
+		diff := Diff{Offset: dstOff, bytes: src.R(0)[srcOff : srcOff+nSize]}
 		b.Diffs = append(b.Diffs, diff)
 	}
-	for _, diff := range src.Diffs {
+	for _, diff := range src.GetDiffs() {
 		if diff.Offset >= srcOff {
 			nDiff := diff
 			nDiff.Offset -= srcOff

query/base/base_test.go
@@ -930,6 +930,337 @@ func (w *MyWriter) WriteAt(p []byte, offset int64) (n int, e error) {
 	return len(p), nil
 }
 
+type BufWriterIO struct {
+	*os.File
+	buf []byte
+	len int
+	cap int
+	pos int64
+	l   int
+}
+
+func NewBufWriterIO(o *os.File, n int) *BufWriterIO {
+
+	return &BufWriterIO{
+		File: o,
+		buf:  make([]byte, 0, n),
+		pos:  int64(0),
+		len:  0,
+		cap:  n,
+		l:    n,
+	}
+}
+
+func (b *BufWriterIO) w() *os.File {
+	return b.File
+}
+
+func (b *BufWriterIO) Write(p []byte) (n int, e error) {
+
+	defer func() {
+		b.len = len(b.buf)
+		b.cap = cap(b.buf)
+	}()
+
+	b.buf = append(b.buf, p[:len(p):len(p)]...)
+	if len(b.buf) >= b.l {
+		n, e = b.w().WriteAt(b.buf, b.pos)
+		if n < 0 || e != nil {
+			return
+		}
+		b.pos += int64(n)
+		b.buf = b.buf[0:0:cap(b.buf)]
+	}
+	return len(p), nil
+
+}
+
+func (b *BufWriterIO) WriteAt(p []byte, offset int64) (n int, e error) {
+
+	defer func() {
+		b.len = len(b.buf)
+		b.cap = cap(b.buf)
+	}()
+
+	if (offset-b.pos)+int64(len(p)) > int64(cap(b.buf)) {
+		nbuf := make([]byte, (offset-b.pos)+int64(len(p)))
+		copy(nbuf[:len(b.buf)], b.buf)
+		b.buf = nbuf
+	}
+	wpos := offset - b.pos
+	copy(b.buf[wpos:wpos+int64(len(p))], p[:len(p):cap(p)])
+	b.buf = b.buf[:wpos+int64(len(p))]
+
+	if len(b.buf) >= b.l {
+		n, e = b.w().WriteAt(b.buf, b.pos)
+		if n < 0 || e != nil {
+			return
+		}
+		b.pos += int64(n)
+		b.buf = b.buf[0:0:cap(b.buf)]
+	}
+	return len(p), nil
+}
+
+func (b *BufWriterIO) Flush() (e error) {
+
+	defer func() {
+		b.len = len(b.buf)
+		b.cap = cap(b.buf)
+	}()
+	var n int
+	if len(b.buf) > 0 {
+		n, e = b.w().WriteAt(b.buf, b.pos)
+		b.pos += int64(n)
+		b.buf = b.buf[n:n]
+	}
+	return
+}
+func (b *BufWriterIO) ReOpen() {
+	name := b.File.Name()
+	b.Flush()
+	b.Close()
+	b.File, _ = os.OpenFile(name, os.O_RDWR, os.ModePerm)
+}
+
+func Benchmark_DirectBuf(b *testing.B) {
+
+	// b.ResetTimer()
+	// b.Run("directbuf block=4k disable=merge", func(b *testing.B) {
+	// 	b.StartTimer()
+	// 	MakeDirectBufFileList(b.N, 4096, false)
+	// 	b.StopTimer()
+	// })
+
+	// b.ResetTimer()
+	// b.Run("directbuf block=4k*8 disable=merge", func(b *testing.B) {
+	// 	b.StartTimer()
+	// 	MakeDirectBufFileList(b.N, 4096*8, false)
+	// 	b.StopTimer()
+	// })
+
+	// b.ResetTimer()
+	// b.Run("directbuf block=4k layer merge-interval-8", func(b *testing.B) {
+	// 	b.StartTimer()
+	// 	MakeDirectBufFileList(loopCnt(b.N), blockSize(4096), useMerge(true), mInterval(8), useNoLayer(false))
+	// 	b.StopTimer()
+	// })
+
+	// b.ResetTimer()
+	// b.Run("directbuf block=512 layer merge-interval-8", func(b *testing.B) {
+	// 	b.StartTimer()
+	// 	MakeDirectBufFileList(loopCnt(b.N), blockSize(512), useMerge(true), mInterval(8), useNoLayer(false))
+	// 	b.StopTimer()
+	// })
+
+	// b.ResetTimer()
+	// b.Run("directbuf block=4k  nomerge-interval-8", func(b *testing.B) {
+	// 	b.StartTimer()
+	// 	MakeDirectBufFileList(loopCnt(b.N), blockSize(4096), useMerge(false), mInterval(8), useNoLayer(true))
+	// 	b.StopTimer()
+	// })
+
+	// b.ResetTimer()
+	// b.Run("directbuf block=512  nomerge-interval-8", func(b *testing.B) {
+	// 	b.StartTimer()
+	// 	MakeDirectBufFileList(loopCnt(b.N), blockSize(512), useMerge(false), mInterval(8), useNoLayer(true))
+	// 	b.StopTimer()
+	// })
+
+	b.ResetTimer()
+	b.Run("directbuf block=8k  merge-interval-8", func(b *testing.B) {
+		b.StartTimer()
+		MakeDirectBufFileList(loopCnt(b.N), blockSize(8192), useMerge(true), mInterval(8), useNoLayer(true))
+		b.StopTimer()
+	})
+
+	b.ResetTimer()
+	b.Run("directbuf block=4k  merge-interval-8", func(b *testing.B) {
+		b.StartTimer()
+		MakeDirectBufFileList(loopCnt(b.N), blockSize(4096), useMerge(true), mInterval(8), useNoLayer(true))
+		b.StopTimer()
+	})
+
+	b.ResetTimer()
+	b.Run("directbuf block=512  merge-interval-8", func(b *testing.B) {
+		b.StartTimer()
+		MakeDirectBufFileList(loopCnt(b.N), blockSize(512), useMerge(true), mInterval(8), useNoLayer(true))
+		b.StopTimer()
+	})
+
+	b.ResetTimer()
+	b.Run("directbuf block=4k merge-interval-4", func(b *testing.B) {
+		b.StartTimer()
+		MakeDirectBufFileList(loopCnt(b.N), blockSize(4096), useMerge(true), mInterval(4), useNoLayer(true))
+
+		b.StopTimer()
+	})
+
+	b.ResetTimer()
+	b.Run("directbuf block=512 merge-interval-4", func(b *testing.B) {
+		b.StartTimer()
+		MakeDirectBufFileList(loopCnt(b.N), blockSize(512), useMerge(true), mInterval(4), useNoLayer(true))
+		b.StopTimer()
+	})
+
+	b.ResetTimer()
+	b.Run("directbuf block=4k merge-interval-2", func(b *testing.B) {
+		b.StartTimer()
+		MakeDirectBufFileList(loopCnt(b.N), blockSize(4096), useMerge(true), mInterval(2), useNoLayer(true))
+
+		b.StopTimer()
+	})
+
+	b.ResetTimer()
+	b.Run("directbuf block=512 merge-interval-2", func(b *testing.B) {
+		b.StartTimer()
+		MakeDirectBufFileList(loopCnt(b.N), blockSize(512), useMerge(true), mInterval(2), useNoLayer(true))
+		b.StopTimer()
+	})
+
+}
+
+type Conf struct {
+	loopCnt    int
+	block      int
+	useMerge   bool
+	mInterval  int
+	useNoLayer bool
+}
+
+type Config func(*Conf)
+
+func loopCnt(i int) Config {
+	return func(c *Conf) {
+		c.loopCnt = i
+	}
+}
+
+func blockSize(i int) Config {
+	return func(c *Conf) {
+		c.block = i
+	}
+}
+
+func useMerge(useMerge bool) Config {
+	return func(c *Conf) {
+		c.useMerge = useMerge
+	}
+}
+
+func useNoLayer(useNoLayer bool) Config {
+	return func(c *Conf) {
+		c.useNoLayer = useNoLayer
+	}
+}
+
+func mInterval(mInterval int) Config {
+	return func(c *Conf) {
+		c.mInterval = mInterval
+	}
+}
+
+func (c *Conf) Apply(confs ...Config) {
+	for i := range confs {
+		confs[i](c)
+	}
+}
+
+func MakeDirectBufFileList(confs ...Config) {
+	opt := Conf{useNoLayer: false}
+
+	opt.Apply(confs...)
+
+	root := query2.NewRoot()
+	root.WithHeader()
+	hoges := query2.NewHoges()
+
+	flist := query2.NewFileList()
+	if opt.useNoLayer {
+		flist.BaseToNoLayer()
+	}
+
+	for i := 0; i < 1; i++ {
+		file := query2.NewFile()
+		if opt.useNoLayer {
+			file.BaseToNoLayer()
+		}
+		file.SetId(query2.FromUint64(10 + uint64(i)))
+		file.SetIndexAt(query2.FromInt64(2000 + int64(i)))
+		file.SetName(base.FromBytes([]byte("namedayo")))
+		flist.SetAt(i, file)
+	}
+
+	flist.Merge()
+	hoges.SetFiles(flist.CommonNode)
+	root.SetIndex(hoges.CommonNode)
+	root.Merge()
+
+	fname := fmt.Sprintf("bench-%d", opt.loopCnt)
+	f, _ := os.Create(fname)
+	defer func() {
+		f.Close()
+		os.Readlink(fname)
+	}()
+	w := NewBufWriterIO(f, opt.block)
+
+	cl := base.CommonList{}
+
+	cl.CommonNode = flist.CommonNode
+	cl.SetDataWriter(w)
+	cl.WriteDataAll()
+	cnt := cl.Count()
+
+	for i := 0; i < opt.loopCnt; i++ {
+		file := query2.NewFile()
+		if opt.useNoLayer {
+			file.BaseToNoLayer()
+		}
+		file.SetId(query2.FromUint64(20 + uint64(i)))
+		file.SetIndexAt(query2.FromInt64(3000 + int64(i)))
+		file.SetName(base.FromBytes([]byte("namedayoadd")))
+		if opt.useMerge {
+			file.Merge()
+		}
+		cl.SetAt(cnt+i, file.CommonNode)
+		if opt.useMerge && i%opt.mInterval == (opt.mInterval-1) {
+			cl.Merge()
+		}
+		if i%100 == 50 {
+			w.ReOpen()
+		}
+
+	}
+
+	cl.Merge()
+
+	bytes := root.R(0)
+
+	pos := root.Index().Hoges().Files().CommonNode.NodeList.ValueInfo.Pos - 4
+	headsize := int(cl.VLen())*4 + 4
+	if len(bytes) < pos+headsize {
+		nbytes := make([]byte, pos+headsize)
+		copy(nbytes, bytes)
+		bytes = nbytes
+	}
+
+	copy(bytes[pos:pos+headsize], cl.R(cl.NodeList.ValueInfo.Pos - 4)[:headsize])
+	bytes = bytes[: pos+headsize : pos+headsize]
+
+	root.Base = base.NewDirectReader(base.NewBase(bytes), w)
+	w.Flush()
+}
+
+func TestCast(t *testing.T) {
+
+	// hoge := interface{}(BaseP{})
+	// _, ok := Base(hoge)
+	// assert.True(t, ok)
+	// hoge = interface{}(&BaseD{})
+	// _, ok = Base(hoge)
+	// assert.True(t, ok)
+}
+
 func Test_CommonList(t *testing.T) {
 
 	//files := query2.NewFiles()
@@ -938,9 +1269,12 @@ func Test_CommonList(t *testing.T) {
 	hoges := query2.NewHoges()
 
 	flist := query2.NewFileList()
+	flist.Merge()
+	flist.BaseToNoLayer()
 
 	for i := 0; i < 3; i++ {
 		file := query2.NewFile()
+		file.BaseToNoLayer()
 		file.SetId(query2.FromUint64(10 + uint64(i)))
 		file.SetIndexAt(query2.FromInt64(2000 + int64(i)))
 		file.SetName(base.FromBytes([]byte("namedayo")))
@@ -951,8 +1285,20 @@ func Test_CommonList(t *testing.T) {
 	root.SetIndex(hoges.CommonNode)
 	root.Merge()
 
+	loopCnt := 10
+
 	w := &MyWriter{}
-	w.Buf = make([]byte, 0, 4096)
+	w.Buf = make([]byte, 0, 4096*loopCnt)
+
+	// fname := fmt.Sprintf("bench-%d", loopCnt)
+	// f, _ := os.Create(fname)
+	// w := NewBufWriterIO(f, 4096)
+	// //w.ReOpen()
+	// defer func() {
+	// 	w.Flush()
+	// 	f.Close()
+	// 	os.Readlink(fname)
+	// }()
 
 	cl := base.CommonList{}
 
@@ -968,11 +1314,13 @@ func Test_CommonList(t *testing.T) {
 	cl.WriteDataAll()
 	cnt := cl.Count()
 
-	for i := 0; i < 3; i++ {
+	for i := 0; i < loopCnt; i++ {
 		file := query2.NewFile()
+		file.BaseToNoLayer()
 		file.SetId(query2.FromUint64(20 + uint64(i)))
 		file.SetIndexAt(query2.FromInt64(3000 + int64(i)))
 		file.SetName(base.FromBytes([]byte("namedayoadd")))
+		file.Merge()
 		cl.SetAt(cnt+i, file.CommonNode)
 	}
 
@@ -982,12 +1330,23 @@ func Test_CommonList(t *testing.T) {
 
 	pos := root.Index().Hoges().Files().CommonNode.NodeList.ValueInfo.Pos - 4
 	headsize := int(cl.VLen())*4 + 4
+	if len(bytes) < pos+headsize {
+		nbytes := make([]byte, pos+headsize)
+		copy(nbytes, bytes)
+		bytes = nbytes
+	}
+
 	copy(bytes[pos:pos+headsize], cl.R(cl.NodeList.ValueInfo.Pos - 4)[:headsize])
 	bytes = bytes[: pos+headsize : pos+headsize]
 
 	root.Base = base.NewDirectReader(base.NewBase(bytes), w)
 
-	assert.Equal(t, uint32(6), root.Index().Hoges().Files().VLen())
+	assert.True(t, uint32(6) < root.Index().Hoges().Files().VLen())
+	// w.Flush()
+	// info, _ := w.File.Stat()
+	// size := root.LenBuf()
+	// _ = size
+	// assert.True(t, 100 < info.Size())
 
 	flist = query2.NewFileList()
 

query/base/list.go
@@ -55,7 +55,7 @@ func (l *CommonList) WriteDataAll() (e error) {
 	//remove writed data area
 	bytes := l.R(0)
 	bytes = bytes[0 : first.Node.Pos-vSize : first.Node.Pos-vSize]
-	l.Base = NewBase(bytes)
+	l.Base = l.Base.New(bytes)
 
 	return nil
 

query/base/node.go
@@ -343,3 +343,7 @@ func (node *Node) Table(idx int) int {
 func (node *NodeList) clearValueInfoOnDirty() {
 	node.ClearValueInfoOnDirty(node)
 }
+
+func (node *Node) BaseToNoLayer() {
+	node.Base = NewNoLayer(node.Base)
+}

commit 962b510d8f1a92f486418581563193c578db73bc

FIx cannot get required-buffer in NoLayer.D() - required-limit is 512.
fixes: ec9d4af

query/base/directreader.go
@@ -142,7 +142,7 @@ func (b NoLayer) D(off, size int) *Diff {
 		return &Diff{Offset: off, bytes: b.bytes[off:]}
 	}
 
-	newDiff := Diff{Offset: off, bytes: make([]byte, 0, 512)}
+	newDiff := Diff{Offset: off, bytes: make([]byte, 0, MaxInt(DEFAULT_BUF_CAP, size))}
 	newDiff.bytes = newDiff.bytes[:size]
 	b.Diffs = append(b.Diffs, newDiff)
 